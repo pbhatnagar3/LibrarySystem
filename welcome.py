4593cfff8cce00cc7c8d230fb066628a16ac369f
Rohit Pathak <rohitpathak.3@gmail.com>
2015-04-20 01:11:34 -0400

    Base html and home page

--- welcome.py
+++ welcome.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
-import MySQLdb
+import mysql.connector
 
 
-db = MySQLdb.connect(host="localhost", # your host, usually localhost
-                     user="root", # your username
-                      passwd="", # your password
-                      db="library") # name of the data base
+# db = MySQLdb.connect(host="localhost", # your host, usually localhost
+#                      user="root", # your username
+#                       passwd="", # your password
+#                       db="library") # name of the data base
 
-cur = db.cursor()
+# cur = db.cursor()
 # from sqlalchemy import create_engine, MetaData
 
 # engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
@@ -24,7 +24,7 @@ def testDB():
 @app.route('/')
 def library_system():
 	school_name = 'Georgia Tech'
-	return render_template('welcome.html', school_name = school_name)
+	return render_template('index.html', school_name = school_name)
 
 @app.route('/hello')
 def hello_world():
